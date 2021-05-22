400d05a514b58209b03f7051fcc4286a6e985b9e
Danny-Yxzl <75941562+Danny-Yxzl@users.noreply.github.com>
2021-05-22 12:46:15 +0800

    Update Version 0.1.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bilnn',
-    version='0.1.0',
+    version='0.1.1',
     description='A SDK For Bilnn Pan',
     py_modules=["bilnn"],
 
