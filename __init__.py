cbb6aa1b402aae3c35da188e148e3dfe7ddf5c56
Danny-Yxzl <75941562+Danny-Yxzl@users.noreply.github.com>
2021-05-22 12:00:26 +0800

    Update to version 0.1.0.

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from bilnn import Bilnn
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bilnn',
-    version='0.0.2',
+    version='0.1.0',
     description='A SDK For Bilnn Pan',
     py_modules=["bilnn"],
 
